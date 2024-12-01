bc8c97b8e22cac9ef25e6686eff2dc989debbf7b
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2024-12-01 21:38:09 +0000

    wrote some handlers for reset which sets the mainpage hits to 0, metrics which tells us the number of hits, and a middleware for the homepage handler that counts how many times it serves a response

--- main.go
+++ main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
-func handlerHealthz(headerMap http.ResponseWriter, req *http.Request) {
-	headerMap.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	headerMap.WriteHeader(http.StatusOK)
-	headerMap.Write([]byte("OK"))
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsIncrease(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerHealthz(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	val := int(cfg.fileServerHits.Load())
+	writeStr := fmt.Sprintf("Hits: %v", strconv.Itoa(val))
+	w.Write([]byte(writeStr))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Store(0)
 }
 
 func main() {
 	sMux := http.NewServeMux()
-
+	var state apiConfig
 	server := http.Server{
 		Handler: sMux,
 		Addr:    ":8080",
 	}
-	sMux.Handle("/", http.FileServer(http.Dir(".")))
-	sMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	sMux.HandleFunc("/healthz", handlerHealthz)
+	sMux.HandleFunc("/metrics", state.handlerMetrics)
+	sMux.HandleFunc("/reset", state.handlerReset)
+	sMux.Handle("/app/", state.middlewareMetricsIncrease(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	server.ListenAndServe()
 }
